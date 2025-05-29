403d5005e4f076d7017a558ce1ebbdd217674ee6
DanishCodebase <ammarsaifi5376@gmail.com>
2025-05-29 15:02:02 +0530

    Initial commit with project setup including configuration files, basic structure, and components for a React application using Vite, Tailwind CSS, and Radix UI. Added .gitignore, ESLint configuration, and essential pages for the online MBA program.

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import Hero from "@/pages/Home/Hero";
+import WhoShouldEnroll from "@/pages/Home/WhoShouldEnroll";
+import Universities from "@/pages/Home/Universities";
+import Benefits from "@/pages/Home/Benefits";
+import FAQ from "@/pages/Home/FAQ";
+import AdmissionQuery from "@/pages/Home/AdmissionQuery";
+
+const App = () => {
+  const [utmParams, setUtmParams] = useState({
+    source: "",
+    medium: "",
+    campaign: "",
+  });
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    setUtmParams({
+      source: urlParams.get("utm_source") || "",
+      medium: urlParams.get("utm_medium") || "",
+      campaign: urlParams.get("utm_campaign") || "",
+    });
+  }, []);
+  return (
+    <div>
+      <AdmissionQuery utmParams={utmParams} />
+      <Hero />
+      <Universities />
+      <WhoShouldEnroll />
+      <Benefits />
+      <FAQ />
+    </div>
+  );
+};
+
+export default App;
